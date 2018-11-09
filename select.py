c196251678c1c233937adbc2e91a26b42ffbeb73
jeffrey levesque <jeff1evesque@yahoo.com>
2018-11-08 19:28:56 -0500

    "#21: select.py, convert logic into function"

--- select.py
+++ select.py
@@ -15,21 +15,22 @@ from config import (
     collection
 )
 
-# endpoint
-client = MongoClient('{}:{}'.format(
-    mongos_endpoint,
-    mongos_port
-))
+def select():
+    # endpoint
+    client = MongoClient('{}:{}'.format(
+        mongos_endpoint,
+        mongos_port
+    ))
 
-# database + collection
-db = client[database]
-col = db[collection]
+    # database + collection
+    db = client[database]
+    col = db[collection]
 
-# select data
-data = []
-cursor = col.find({})
-for document in cursor:
-    data.append(document)
+    # select data
+    data = []
+    cursor = col.find({})
+    for document in cursor:
+        data.append(document)
 
-# return data
-return(data)
+    # return data
+    return(data)
