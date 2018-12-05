c72000bc02d82571ddcdedd409c3baa6aaded0bc
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-04 19:29:53 -0500

    #29: select.py, fix mapreduce

--- app/select.py
+++ app/select.py
@@ -25,15 +25,15 @@ def select(client, database, collection):
           emit(
             this.link_id,
             {
-              'id': this.id,
-              'score': this.score,
-              'parent_id': this.parent_id,
-              'body': this.body,
-              'comment': this.body,
-            }"
-          );"
-        '''}
-    )
+              'id': [this.id],
+              'score': [this.score],
+              'parent_id': [this.parent_id],
+              'body': [this.body],
+              'comment': [this.body]
+            }
+          );
+        }
+    ''')
 
     #
     # @to (key), the parent post
@@ -41,10 +41,38 @@ def select(client, database, collection):
     #
     reduce = Code('''
         function (key, values) {
-          var results = { posts: [], comments: [], match_id: [], score: [], link_id: key };
-          return {values};
-        '''}
-    )
+          var results = { posts: [], comments: [], match_id: [], score: [] };
+          for (var i = 0; i < values.length; i++) {
+            if (
+              values[i] &&
+              values[i].body &&
+              values[i].parent_id &&
+              values[i].body[0].trim() != '[deleted]' &&
+              values[i].body[0].trim() != '[removed]'
+            ) {
+              var comment = values[i].body;
+              var score = values[i].score;
+              var wantedParent = values[i].parent_id[0].split('_')[1];
+              for (var j = 0; j < values.length; j++) {
+                if (
+                    values[j] &&
+                    values[j].body &&
+                    values[j].body != values[i].body &&
+                    values[j].body[0].trim() != '[deleted]' &&
+                    values[j].body[0].trim() != '[removed]' &&
+                    wantedParent == values[j].id
+                ) {
+                  results.posts = results.posts.concat(values[j].body);
+                  results.match_id = results.match_id.concat(wantedParent);
+                  results.comments = results.comments.concat(comment);
+                  results.score = results.score.concat(comment);
+                }
+              }
+            }
+          }
+          return { results };
+        }
+    ''')
 
     # select data
     return col.map_reduce(map, reduce, 'to_from')
