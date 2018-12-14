8c069ecf021d5348368681b0791d25f3deff4f63
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-13 19:29:40 -0500

    #62: entrypoint.py, apply penn-tree tokenization

--- chatbot/entrypoint.py
+++ chatbot/entrypoint.py
@@ -7,7 +7,7 @@ upload.py, upload specified data to mongodb endpoint.
 '''
 
 import os
-from nltk.corpus import treebank
+from nltk import tag, word_tokenize
 from nmt_chatbot.inference import interactive
 
 original_cwd = os.getcwd()
@@ -15,10 +15,18 @@ print("\n\nStarting interactive mode (first response will take a while):")
 
 # QAs
 while True:
+    # prompt input
     question = input('\n> ')
-    print(interactive(question))
+
+    # penn-tree: tokenize + parts of speech
+    sent = word_tokenize(question)
+    tagged_words = tag.pos_tag(sent)
+
+    # generate response
     inference_internal = interactive(question)
     answers = inference_internal(question)[0]
+
+    # display response
     if answers is None:
         print(colorama.Fore.RED + "! Question can't be empty" + colorama.Fore.RESET)
     else:
