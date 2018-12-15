3099fb4d56b1c17bcdc4ab4b023e1e594a6558c9
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-14 19:27:18 -0500

    #64: fix syntax typos

--- QuestionAnswerCMU/utility.py
+++ QuestionAnswerCMU/utility.py
@@ -6,7 +6,7 @@ utility.py, helper function.
 
 '''
 
-def normalize_data(X_train, X_test, stop_gap=40):
+def normalize_data(X_train, X_test, stop_gap=40, stop_value=0):
     ## get shape of dataframe
     rows_train, columns_train = X_train.shape
     rows_test, columns_test = X_test.shape
@@ -21,15 +21,19 @@ def normalize_data(X_train, X_test, stop_gap=40):
     ##
     if train_delta > 0 and train_delta <= stop_gap:
         for i in range(train_delta):
-            X_train['filler-{}'.format(i)] = stop_gap
+            X_train['filler-{}'.format(i)] = stop_value
 
     if test_delta > 0 and test_delta <= stop_gap:
         for i in range(test_delta):
-            X_test['filler-{}'.format(i)] = stop_gap
+            X_test['filler-{}'.format(i)] = stop_value
 
     if train_delta < 0:
         rem_list = [x for x in range(abs(columns_train))][train_delta:]
         X_train.drop(rem_list, axis=1, inplace=True)
+
+    if test_delta < 0:
+        rem_list = [x for x in range(abs(columns_train))][test_delta:]
+        X_test.drop(rem_list, axis=1, inplace=True)
 
     ## return train + test
     return(X_train, X_test)
