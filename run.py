c98ba257910cc34dadcbebb901430bbe14976682
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-17 18:25:39 -0500

    #71: run.py, decrease SO threshold

--- run.py
+++ run.py
@@ -54,7 +54,7 @@ while True:
         # display response
         if answers is None:
             print(colorama.Fore.RED + "! Question can't be empty" + colorama.Fore.RESET)
-        elif answers['best_score'] < 15:
+        elif answers['best_score'] < 20:
             print('hey {name}, maybe checkout {url}'.format(
                 name=client,
                 url=so_model(cwd).predict([sentence])
