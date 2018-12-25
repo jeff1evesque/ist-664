fb67cd994e2efd3fe91fa6d55536bc1de850c74d
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-25 12:49:42 -0500

    #74: predict.py, implement + define 'decode'

--- chatbot/app/predict.py
+++ chatbot/app/predict.py
@@ -7,6 +7,7 @@ predict.py, using loaded lstm model generate sequence.
 '''
 
 from keras.models import load_model
+from joblib import load
 
 
 def predict(cwd, question):
@@ -17,4 +18,16 @@ def predict(cwd, question):
     '''
 
     model = load_model('{base}/model/chatbot.h5'.format(base=cwd))
-    return model.predict(question)
+    return model.predict(decode(question), cwd=cwd)
+
+def decode(indices, calc_argmax=True, cwd=''):
+    '''
+
+    convert indices to words.
+
+    '''
+
+    idx2word = load('{base}/model/idx2word.pkl'.format(base=cwd))
+    if calc_argmax:
+        indices = np.argmax(indices, axis=-1)
+    return ' '.join(idx2word[x] for x in indices)
