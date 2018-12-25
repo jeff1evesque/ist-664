80897c7d2aa44bf0f0cc77b48aa15fe5662d20df
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-25 11:36:24 -0500

    #74: config-TEMPLATE.py, add 'username' change 'mongos_endpoint'

--- chatbot/config-TEMPLATE.py
+++ chatbot/config-TEMPLATE.py
@@ -6,8 +6,8 @@ config-TEMPLATE.py, RENAME TO config.py, AND MAKE ADJUSTMENTS AS NEEDED.
 
 '''
 
-mongos_endpoint = ['xxx.xxx.xxx.xxx', 'xxx.xxx.xxx.xxx']
-mongos_port = 27017
+mongos_endpoint = ['xxx.xxx.xxx.xxx:yyyy', 'xxx.xxx.xxx.xxx:yyyy']
 database = 'DATABASE-NAME'
 collection = 'COLLECTION-NAME'
 data_directory = 'data'
+username='jimmy'
