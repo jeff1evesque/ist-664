1358134aa6ee4f3df6dff3d77d5cd270ebcdb69e
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-29 22:26:05 -0500

    #91: config-TEMPLATE.py, parameterize neural network attributes

--- Reddit/config-TEMPLATE.py
+++ Reddit/config-TEMPLATE.py
@@ -6,8 +6,22 @@ config-TEMPLATE.py, RENAME TO config.py, AND MAKE ADJUSTMENTS AS NEEDED.
 
 '''
 
+# general
 mongos_endpoint = ['xxx.xxx.xxx.xxx:yyyy', 'xxx.xxx.xxx.xxx:yyyy']
 database = 'DATABASE-NAME'
 collection = 'COLLECTION-NAME'
 data_directory = 'data'
 username='jimmy'
+
+# recurrent neural network
+epochs = 1
+batch_size = 32
+split = 0.2
+n_hidden = 128
+post_maxlen = 10
+comment_maxlen = 20
+dropout = 0.2
+recurrent_dropout = 0.2
+checkpoint = False
+checkpoint_period = 1
+cwd = '~'
