b96ec5c081082a87ed93dacffed4c0dd21ee7730
jeffrey levesque <jeff1evesque@yahoo.com>
2018-12-29 22:23:19 -0500

    #91: implement vectorization for prediction

--- Reddit/app/predict.py
+++ Reddit/app/predict.py
@@ -7,27 +7,47 @@ predict.py, using loaded lstm model generate sequence.
 '''
 
 from keras.models import load_model
+import numpy as np
 from joblib import load
+from Reddit.app.train import create_posts
 
 
-def predict(cwd, question):
+def predict(post, post_maxlen, cwd=''):
     '''
 
     predict sequence response.
 
     '''
 
-    model = load_model('{base}/Reddit/model/chatbot.h5'.format(base=cwd))
-    return model.predict(decode(question), cwd=cwd)
+    # load model + converter
+    model = load_model('{base}/Reddit/model/rnn_lstm.h5'.format(base=cwd))
+    word2idx = load('{base}/Reddit/model/word2idx.pkl'.format(base=cwd))
 
-def decode(indices, calc_argmax=True, cwd=''):
+    # vectorize post
+    vocab_size = len(word2idx) + 1
+    vectorized = create_posts(post, vocab_size, post_maxlen, word2idx)
+
+    # predict reply
+    comment = model.predict(vectorized)
+    return(decode(comment, cwd))
+
+def decode(posts, cwd, calc_argmax=True):
     '''
 
     convert indices to words.
 
     '''
 
+    # local variables
+    result = []
+
+    # load indexer
     idx2word = load('{base}/Reddit/model/idx2word.pkl'.format(base=cwd))
     if calc_argmax:
-        indices = np.argmax(indices, axis=-1)
-    return ' '.join(idx2word[x] for x in indices)
+        posts = np.argmax(posts, axis=-1)
+
+    # convert index to words
+    for sentence in posts:
+        result.append(' '.join(idx2word[x] for x in sentence))
+
+    return result
